38a27252065da2c1ecb3716b6590498f167be9cd
Michael Lutsenko <Itheadon@users.noreply.github.com>
2020-09-09 17:07:48 +0300

    Add styling to RepositoryList

--- src/repositories/RepositoryList.tsx
+++ src/repositories/RepositoryList.tsx
@@ -3,18 +3,51 @@
 import React from 'react';
 import { RepositoryInfo } from '../api/types';
 
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+  Link,
+} from '@material-ui/core';
+import { Star, Restaurant } from '@material-ui/icons';
+
 interface RepositoryListProps {
   items: [RepositoryInfo];
 }
 
 const RepositoryList = ({ items }: RepositoryListProps) => (
-  <React.Fragment>
-    {items.map((repo: RepositoryInfo) => (
-      <div>
-        {repo.name}, {repo.url}, {repo.stargazers.totalCount}, {repo.forkCount}
-      </div>
-    ))}
-  </React.Fragment>
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell align="right">
+            <Star />
+            &nbsp;Stars
+          </TableCell>
+          <TableCell align="right">
+            <Restaurant />
+            &nbsp;Forks
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {items.map((repo: RepositoryInfo, index: number) => (
+          <TableRow key={`repo_row_${index}`}>
+            <TableCell>
+              <Link href={repo.url}>{repo.nameWithOwner}</Link>
+            </TableCell>
+            <TableCell align="right">{repo.stargazers.totalCount}</TableCell>
+            <TableCell align="right">{repo.forkCount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
 );
 
 export default RepositoryList;
